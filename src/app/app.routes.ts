c8b4f9d41728db45baa0773db007c5be030f7e2f
Cristian Aldair Bonifacio Quispe <89119241+CristianBonifacio@users.noreply.github.com>
2024-12-04 16:14:56 -0500

    Trazabilidad de llamadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { TrazabilidadComponent } from './Components/trazabilidad/trazabilidad.component';
+import { HomeComponent } from './Components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      path: 'trazabilidad',  // La URL que activará esta ruta
+      component: TrazabilidadComponent,  // El componente que se renderizará
+    },
+    {
+        path: '',
+        component:HomeComponent
+    }
+  ];
